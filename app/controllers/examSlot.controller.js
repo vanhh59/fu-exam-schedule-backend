5739ec5f4ab573631cf4d476c8522829628dab5a
Nguyễn Quang Khánh <117915893+khaqng@users.noreply.github.com>
2023-10-13 14:44:42 +0700

    Update examSlot.controller.js

--- app/controllers/examSlot.controller.js
+++ app/controllers/examSlot.controller.js
@@ -20,75 +20,14 @@ exports.createExamSlot = async function (req, res) {
     });
 }
 
-exports.search = async function (req, res) {
-    var keyword = req.params.keyword;
-    var pool = await conn;
-    var sqlQuery = "SELECT * FROM ExamSlot WHERE ID LIKE @keyword OR name";
-    return await pool.request()
-        .input('keyword', sql.NVarChar, '%' + keyword + '%')
-        .query(sqlQuery, function (error, data) {
-            if (data.recordset && data.recordset.length > 0) {
-                res.send({
-                    result: data.recordset
-                });
-            } else {
-                res.status(404).send('Không tìm thấy!');
-            }
-        });
-}
-
-exports.sort = async function (req, res) {
-    var sort = req.params.sort;
-    var pool = await conn;
-    var sqlQuery = "SELECT * FROM ExamSlot ORDER BY " + sort;
-    return await pool.request()
-        .query(sqlQuery, function (error, data) {
-            if (data.recordset && data.recordset.length > 0) {
-                res.send({
-                    result: data.recordset
-                });
-            } else {
-                res.status(404).send('Không tìm thấy!');
-            }
-        });
-
-}
-
-exports.updateExamSLot = async function (req, res) {
-    student.update(req.body.ID, req.body, function (err, data) {
+exports.updateExamSlot = async function (req, res) {
+    student.updateExamSLot(req.body.ID, req.body, function (err, data) {
         res.send({ result: data, error: err });
     });
 }
 
-exports.deleteByID = async function (req, res) {
-    var id = req.params.id;
-    var pool = await conn;
-    var sqlQuery = "UPDATE ExamSlot SET status = 0 WHERE ID = @ID";
-    return await pool.request()
-        .input('ID', sql.VarChar, id)
-        .query(sqlQuery)
-        .then(function (data) {
-            res.send({ result: "Xóa thành công" });
-            console.log(data);
-        })
-        .catch(function (error) {
-            res.send({ result: "Xóa thất bại" });
-            console.log(error);
-        });
-}
-exports.deleteAll = async function (req, res) {
-    var id = req.params.id;
-    var pool = await conn;
-    var sqlQuery = "UPDATE ExamSlot SET status = 0";
-    return await pool.request()
-        .input('ID', sql.VarChar, id)
-        .query(sqlQuery)
-        .then(function (data) {
-            res.send({ result: "Xóa thành công" });
-            console.log(data);
-        })
-        .catch(function (error) {
-            res.send({ result: "Xóa thất bại" });
-            console.log(error);
-        });
+exports.deleteExamSlot = async function (req, res) {
+    student.deleteExamSlot(req.body.ID, req.body.status, function (err, data) {
+        res.send({ result: data, error: err });
+    });
 }
