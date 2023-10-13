3c64c0f587293b6b91623edff1a3115c35786e94
Nguyễn Quang Khánh <117915893+khaqng@users.noreply.github.com>
2023-10-13 14:46:39 +0700

    Update department.route.js

--- app/routes/department.route.js
+++ app/routes/department.route.js
@@ -1,31 +1,52 @@
-module.exports = function (app) {
-    var departmentController = require('../controllers/department.controller');
+var { conn, sql } = require('../../connect');
+var Department = require('../models/department.model');
+var department = new Department();
 
-    //route lấy tất cả
-    app.get('/department', departmentController.getListAll);
-
-    //route lấy theo examinerID
-    app.get('/department/:examinerID', departmentController.getDepartmentByExaminerID);
-
-    //route lấy theo loaction
-    app.get('/department/:location', departmentController.getByLocation);
-
-    //route lấy theo phone
-    app.get('/department/:examinerID', departmentController.getByPhone);
-
-    //route lấy theo name
-    app.get('/department/:name', departmentController.getByName);
-
-    //route thêm mới
-    app.post('/department', departmentController.createDepartment);
-
-    //route update
-    app.put('/department/', departmentController.updateDepartment);
-
-    //route xóa theo name
-    app.delete('/department/:name', departmentController.deleteDepartment);
-
-    //route xóa tất cả
-    app.delete('/department', departmentController.deleteAll);
+exports.getListAll = async function (req, res) {
+    department.getAll(function (err, data) {
+        res.send({ result: data, error: err });
+    });
 }
 
+exports.getDepartmentByExaminerID = async function (req, res) {
+    department.getDepartmentByExaminerID(req.params.examinerID, function (err, data) {
+        res.send({ result: data, error: err });
+    });
+}
+
+exports.getByLocation = async function (req, res) {
+    department.getByLocation(req.params.location, function (err, data) {
+        res.send({ result: data, error: err });
+    });
+}
+
+exports.getByPhone = async function (req, res) {
+    department.getByPhone(req.params.phone, function (err, data) {
+        res.send({ result: data, error: err });
+    });
+}
+
+exports.getByName = async function (req, res) {
+    department.getByName(req.params.name, function (err, data) {
+        res.send({ result: data, error: err });
+    });
+}
+
+exports.createDepartment = async function (req, res) {
+    department.create(req.body, function (err, data) {
+        res.send({ result: data, error: err });
+    });
+}
+
+
+exports.updateDepartment = async function (req, res) {
+    department.update(req.body.ID, req.body, function (err, data) {
+        res.send({ result: data, error: err });
+    });
+}
+
+exports.deleteDepartment = async function (req, res) {
+    department.deleteDepartment(req.body.name, req.body.status, function (err, data) {
+        res.send({ result: data, error: err });
+    });
+}
