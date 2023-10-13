22bbed302bacb5b929b8677ed95fb88e053bab91
Nguyễn Quang Khánh <117915893+khaqng@users.noreply.github.com>
2023-10-13 14:43:34 +0700

    Update department.controller.js

--- app/controllers/department.controller.js
+++ app/controllers/department.controller.js
@@ -38,39 +38,6 @@ exports.createDepartment = async function (req, res) {
     });
 }
 
-exports.search = async function (req, res) {
-    var keyword = req.params.keyword;
-    var pool = await conn;
-    var sqlQuery = "SELECT * FROM Department WHERE name LIKE @keyword OR examinerType LIKE @keyword OR faculty LIKE @keyword OR location LIKE @keyword OR phone LIKE @keyword";
-    return await pool.request()
-        .input('keyword', sql.NVarChar, '%' + keyword + '%')
-        .query(sqlQuery, function (error, data) {
-            if (data.recordset && data.recordset.length > 0) {
-                res.send({
-                    result: data.recordset
-                });
-            } else {
-                res.status(404).send('Không tìm thấy!');
-            }
-        });
-}
-
-exports.sort = async function (req, res) {
-    var sort = req.params.sort;
-    var pool = await conn;
-    var sqlQuery = "SELECT * FROM ExamSlot ORDER BY " + sort;
-    return await pool.request()
-        .query(sqlQuery, function (error, data) {
-            if (data.recordset && data.recordset.length > 0) {
-                res.send({
-                    result: data.recordset
-                });
-            } else {
-                res.status(404).send('Không tìm thấy!');
-            }
-        });
-
-}
 
 exports.updateDepartment = async function (req, res) {
     department.update(req.body.ID, req.body, function (err, data) {
@@ -79,34 +46,7 @@ exports.updateDepartment = async function (req, res) {
 }
 
 exports.deleteDepartment = async function (req, res) {
-    var name = req.params.name;
-    var pool = await conn;
-    var sqlQuery = "UPDATE Department SET status = 0 WHERE name = @name";
-    return await pool.request()
-        .input('name', sql.VarChar, name)
-        .query(sqlQuery)
-        .then(function (data) {
-            res.send({ result: "Xóa thành công" });
-            console.log(data);
-        })
-        .catch(function (error) {
-            res.send({ result: "Xóa thất bại" });
-            console.log(error);
-        });
-}
-exports.deleteAll = async function (req, res) {
-    var name = req.params.name;
-    var pool = await conn;
-    var sqlQuery = "UPDATE Department SET status = 0";
-    return await pool.request()
-        .input('name', sql.VarChar, name)
-        .query(sqlQuery)
-        .then(function (data) {
-            res.send({ result: "Xóa thành công" });
-            console.log(data);
-        })
-        .catch(function (error) {
-            res.send({ result: "Xóa thất bại" });
-            console.log(error);
-        });
+    department.deleteDepartment(req.body.name, req.body.status, function (err, data) {
+        res.send({ result: data, error: err });
+    });
 }
