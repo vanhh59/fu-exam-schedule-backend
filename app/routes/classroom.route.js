9bcd0b0ec94870d285365892bba0e5f4ba06e749
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-16 10:35:50 +0700

    Add files via upload

---
+++ app/routes/classroom.route.js
@@ -0,0 +1,19 @@
+module.exports = function (app) {
+    var classRoomController = require('.../controllers/classroom.controller');
+
+    //route lấy tất cả
+
+    app.get('/classroom',classRoomController.getListAll);
+    //route lấy theo id
+
+    app.get('/classroom/:id',classRoomController.getListByID);
+    //route thêm mới
+
+    app.post('/classroom', classRoomController.createClassroom);
+    //route update
+
+    app.put('/classroom/', classRoomController.updateClassroom);
+
+    //route xóa theo id
+    app.delete('/classroom/:id', classRoomController.deleteClassroom);
+}
