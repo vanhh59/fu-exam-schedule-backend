acb499c69be71f16962ff3cb7dfdd35045dd787c
Nguyễn Quang Khánh <khanhquangnguyennqk@gmail.com>
2023-10-28 19:50:10 +0700

    feature/Dev_Module_Authorize
    Các tính năng cập nhật
    - Cập nhật Database

--- connect.js
+++ connect.js
@@ -1,26 +1,56 @@
-var sql = require('mssql/msnodesqlv8');
+// var sql = require('mssql/msnodesqlv8');
 
-// các thông tin kết nối csdl
-var config = {
-    // server: "mssql-147764-0.cloudclusters.net,14397",
-    // user: "swp391",
-    // password: "Swp391@@@",
-    // database: "DB_EXAM_1", // Config cho DB dùng ở server
+// // các thông tin kết nối csdl
+// var config = {
+//     // server: "mssql-147764-0.cloudclusters.net,14397",
+//     // user: "swp391",
+//     // password: "Swp391@@@",
+//     // database: "DB_EXAM_1", // Config cho DB dùng ở server
 
-    server: ".",
-    user: "sa",
-    password: "123456",
-    database: "DB_EXAM_1", // Config cho DB dùng ở local
+//     server: ".",
+//     user: "sa",
+//     password: "123456",
+//     database: "DB_EXAM_1", // Config cho DB dùng ở local
     
-    driver: "msnodesqlv8"
+//     driver: "msnodesqlv8"
+// };
+
+// // tạo pool kết nối tới csdl
+// var conn = new sql.ConnectionPool(config).connect().then(pool => {
+//     return pool;
+// });
+// //xuất ra để các file khác có thể sử dụng
+// module.exports = {
+//     conn: conn,
+//     sql: sql
+// }
+
+
+var sql = require('mssql');
+
+var config = {
+    server: "mssql-147764-0.cloudclusters.net",
+    port: 14397,
+    user: "swp391",
+    password: "Swp391@@@",
+    database: "DB_EXAM_1",
+    driver: "tedious",
+    options: {
+        trustServerCertificate: true
+    }
 };
 
-// tạo pool kết nối tới csdl
-var conn = new sql.ConnectionPool(config).connect().then(pool => {
-    return pool;
-});
-//xuất ra để các file khác có thể sử dụng
+var conn = new sql.ConnectionPool(config).connect()
+    .then(pool => {
+        console.log("Database connection is successful.");
+        return pool;
+    })
+    .catch(err => {
+        console.error("Database connection failed:", err);
+    });
+
 module.exports = {
     conn: conn,
     sql: sql
-}
+};
+
