e4c4529918acf2b37ba323998f28cd7c6b9d7f97
Lương Phạm Việt Anh <vanhhh59@gmail.com>
2023-11-02 08:41:19 +0700

    Hot Fix 2

--- connect.js
+++ connect.js
@@ -2,30 +2,30 @@ var sql = require('mssql');
 
 
 // Config Kết nối database server
-// var config = {
-//     server: "mssql-147764-0.cloudclusters.net",
-//     port: 14397,
-//     user: "swp391",
-//     password: "Swp391@@@",
-//     database: "DB_EXAM_2",
-//     driver: "tedious",
-//     options: {
-//         trustServerCertificate: true
-//     }
-// };
-
-// Config kết nối database local
 var config = {
-    server: "localhost",
-    port: 1433,
-    user: "sa",
-    password: "123456",
+    server: "mssql-147764-0.cloudclusters.net",
+    port: 14397,
+    user: "swp391",
+    password: "Swp391@@@",
     database: "DB_EXAM_2",
     driver: "tedious",
     options: {
         trustServerCertificate: true
     }
 };
+
+// Config kết nối database local
+// var config = {
+//     server: "localhost",
+//     port: 1433,
+//     user: "sa",
+//     password: "123456",
+//     database: "DB_EXAM_2",
+//     driver: "tedious",
+//     options: {
+//         trustServerCertificate: true
+//     }
+// };
 
 var conn = new sql.ConnectionPool(config).connect()
     .then(pool => {
